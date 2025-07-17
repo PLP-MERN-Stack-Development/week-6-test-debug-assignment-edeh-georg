9ea794dc4fa56c4ec6a905f01abc29ea63aa4306
edeh-george <gofy6566@gmail.com>
2025-07-17 04:38:51 +0100

    starting client tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -20,26 +20,26 @@ export default {
     },
     
     // Client-side tests configuration
-  //   {
-  //     displayName: 'client',
-  //     testEnvironment: 'jsdom',
-  //     testMatch: ['<rootDir>/client/src/**/*.test.{js,jsx}'],
-  //     moduleFileExtensions: ['js', 'jsx', 'json'],
-  //     moduleNameMapper: {
-  //       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  //       '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/client/src/tests/__mocks__/fileMock.js',
-  //     },
-  //     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  //     transform: {
-  //       '^.+\\.(js|jsx)$': 'babel-jest',
-  //     },
-  //     coverageDirectory: '<rootDir>/coverage/client',
-  //     collectCoverageFrom: [
-  //       'client/src/**/*.{js,jsx}',
-  //       '!client/src/index.js',
-  //       '!**/node_modules/**',
-  //     ],
-  //   },
+    {
+      displayName: 'client',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/client/src/**/*.test.{js,jsx}'],
+      moduleFileExtensions: ['js', 'jsx', 'json'],
+      moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/client/src/tests/__mocks__/fileMock.js',
+      },
+      // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+      transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+      },
+      coverageDirectory: '<rootDir>/coverage/client',
+      collectCoverageFrom: [
+        'client/src/**/*.{js,jsx}',
+        '!client/src/index.js',
+        '!**/node_modules/**',
+      ],
+    },
   ],
   
   // Global configuration
